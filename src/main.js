81f576ab354cb402fe85deb11ee7f557b2abf101
Michota <dawidmichota@gmail.com>
2023-10-28 14:00:18 +0200

    refactor: move functions to new, separated files

--- src/main.js
+++ src/main.js
@@ -1,29 +1,10 @@
-import { fetchData } from "./fetchData";
 import "./styles/global.css";
+import { renderProducts } from "./utilities/renderProducts";
 
 const sectionProducts = document.querySelector(".section-products");
 const productsContainer = sectionProducts.querySelector(".products-container");
 
-// btn.addEventListener("click", () => {
-//   console.log(getLastItem());
-// });
-
 let wasIntersected = false;
-
-const getLastItem = () =>
-  productsContainer.querySelector(".product-box:last-child");
-
-async function renderProducts(containerElement) {
-  const { data } = await fetchData();
-  data.forEach(function (productData) {
-    const productBox = document.createElement("div");
-    productBox.className = "product-box";
-    productBox.innerHTML = `
-          <span>ID: <span class="product-id">${productData.id}</span></span>
-    `;
-    containerElement.appendChild(productBox);
-  });
-}
 
 // Lazy-Load first data
 async function lazyLoadProducts(payload) {
@@ -40,19 +21,22 @@ async function lazyLoadProducts(payload) {
 const sectionObserver = new IntersectionObserver(lazyLoadProducts);
 sectionObserver.observe(sectionProducts);
 
-// Observe last element
+// Observe last element in container
+
+const getLastItem = () =>
+  productsContainer.querySelector(".product-box:last-child");
+
+function observeLastItem() {
+  lastItemObserver.observe(getLastItem());
+}
 
 // Load new data when last item was seen by user
-async function renderNewData(payload) {
+
+async function infiniteLoader(payload) {
   const { isIntersecting } = payload[0];
   if (isIntersecting) {
     await renderProducts(productsContainer);
     observeLastItem();
   }
 }
-
-const lastItemObserver = new IntersectionObserver(renderNewData);
-
-function observeLastItem() {
-  lastItemObserver.observe(getLastItem());
-}
+const lastItemObserver = new IntersectionObserver(infiniteLoader);
---
+++ src/utilities/renderProducts.js
@@ -0,0 +1,13 @@
+import { fetchData } from "../services/fetchData";
+
+export async function renderProducts(containerElement) {
+  const { data } = await fetchData();
+  data.forEach(function (productData) {
+    const productBox = document.createElement("div");
+    productBox.className = "product-box";
+    productBox.innerHTML = `
+          <span>ID: <span class="product-id">${productData.id}</span></span>
+    `;
+    containerElement.appendChild(productBox);
+  });
+}
